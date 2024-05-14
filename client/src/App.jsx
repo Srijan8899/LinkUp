a55bf94d908cebe87c1e41764b15a2bf6e0be1da
Srijan Agrawal <asrijan889@gmail.com>
2024-05-14 22:14:29 +0530

    create login , signUp page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { lazy } from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Groups = lazy(() => import("./pages/Groups"));
+const Chat = lazy(() => import("./pages/Chat"));
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/chat/:chatId" element={<Chat />} />
+        <Route path="/groups" element={<Groups />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
