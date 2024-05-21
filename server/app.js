a68cfa213ab7100ffc5db27653d960afa3ae3a74
Srijan Agrawal <asrijan889@gmail.com>
2024-05-21 15:41:12 +0530

    Created Some Component at client Side and Created Models in Server Side

--- server/app.js
+++ server/app.js
@@ -1,1 +1,11 @@
-console.log("Hello from app.js!")
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from express!");
+});
+
+app.listen(3000, () => {
+  console.log(`server is running on port 3000`);
+});
---
+++ server/models/message.js
@@ -0,0 +1,35 @@
+import { Schema, models, model, Types } from "mongoose";
+
+const schema = new Schema(
+  {
+    content: String,
+
+    sender: {
+      type: Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    chat: {
+      type: Types.ObjectId,
+      ref: "Chat",
+      required: true,
+    },
+    attachments: [
+      {
+        public_id: {
+          type: String,
+          required: true,
+        },
+        url: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Message = models.Message || model("Message", schema);
